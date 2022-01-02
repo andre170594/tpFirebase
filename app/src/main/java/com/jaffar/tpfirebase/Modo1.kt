a2ff1dea711cb7067f419578bc61bbd84ac198b6
andre170594 <andre_pessoa99@hotmail.com>
2022-01-02 05:41:01 +0000

    rter

--- app/src/main/java/com/jaffar/tpfirebase/Modo1.kt
+++ app/src/main/java/com/jaffar/tpfirebase/Modo1.kt
@@ -7,21 +7,31 @@ import android.view.View
 import android.widget.*
 import androidx.core.view.get
 import com.jaffar.tpfirebase.databinding.ActivityModo1Binding
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class Modo1 : AppCompatActivity(){
 
 
-    private lateinit var bindModo1: ActivityModo1Binding
 
+    private  var vez:Int? = null
+    private lateinit var bindModo1: ActivityModo1Binding
     lateinit var gridView: GridView
     var pecasImages = ArrayList<Int>(64)
+    var pecasChars = ArrayList<Char>(64)
 
+    lateinit var numJogadasPoss:ArrayList<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindModo1 = ActivityModo1Binding.inflate(layoutInflater)
         setContentView(bindModo1.root)
+
+
+        var vez = (1..2).random()
+
+        numJogadasPoss = ArrayList()
 
        for(i in 0 until 64){
            if(i == 27 || i == 36){
@@ -44,9 +54,25 @@ class Modo1 : AppCompatActivity(){
         gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, pos, _ ->
 
 
-            // aqui temos de ver qual é a vez de jogar para escolher qual letra meter
+            verificaVizinhos(pecasImages, vez!!,numJogadasPoss)
 
-            pecasImages.set(pos,R.drawable.x)
+            println(numJogadasPoss)
+            println("size: " + numJogadasPoss.size)
+
+
+            for (i in 0..numJogadasPoss.size){
+                if(pos == i){
+                    if(vez==1){
+                        pecasImages.set(pos,R.drawable.x)
+                        vez=2
+                    }
+                    else {
+                        pecasImages.set(pos,R.drawable.y)
+                        vez=1
+                    }
+                }
+            }
+
 
 
             val pecasAdapter = PecasAdapter(this,pecasImages)
@@ -57,6 +83,51 @@ class Modo1 : AppCompatActivity(){
         }
 
 
+    }
+
+    private fun verificaVizinhos(pecasImages: ArrayList<Int>, vez: Int, numJogadasPoss: ArrayList<Int>) {
+
+        var peca:Int
+        var temp: Int
+
+        if(vez == 1){
+            peca = R.drawable.y
+            temp = R.drawable.y
+        }
+        else{
+            temp = R.drawable.x
+            peca = R.drawable.x
+        }
+
+
+
+
+        for(i in 0..63){
+            if(pecasImages[i] == temp){
+                if(pecasImages[i+1] == peca) verificavizinhos2(i+1,pecasImages,temp,numJogadasPoss)
+                if(pecasImages[i-1] == peca) verificavizinhos2(i-1,pecasImages,temp,numJogadasPoss)
+                if(pecasImages[i+8] == peca) verificavizinhos2(i+8,pecasImages,temp,numJogadasPoss)
+                if(pecasImages[i-8] == peca) verificavizinhos2(i-8,pecasImages,temp,numJogadasPoss)
+                if(pecasImages[i+9] == peca) verificavizinhos2(i+9,pecasImages,temp,numJogadasPoss)
+                if(pecasImages[i-9] == peca) verificavizinhos2(i-9,pecasImages,temp,numJogadasPoss)
+                if(pecasImages[i+7] == peca) verificavizinhos2(i+7,pecasImages,temp,numJogadasPoss)
+                if(pecasImages[i-7] == peca) verificavizinhos2(i-7,pecasImages,temp,numJogadasPoss)
+            }
+        }
+
+    }
+
+    private fun verificavizinhos2(i: Int, pecasImages: ArrayList<Int>, vez: Int, numJogadasPoss: ArrayList<Int>) {
+
+        numJogadasPoss.clear()
+        if(pecasImages[i+1] == vez) numJogadasPoss.add(i-1)
+        if(pecasImages[i-1] == vez) numJogadasPoss.add(i-1)
+        if(pecasImages[i+8] == vez) numJogadasPoss.add(i-1)
+        if(pecasImages[i-8] == vez) numJogadasPoss.add(i-1)
+        if(pecasImages[i+9] == vez) numJogadasPoss.add(i-1)
+        if(pecasImages[i-9] == vez) numJogadasPoss.add(i-1)
+        if(pecasImages[i+7] == vez) numJogadasPoss.add(i-1)
+        if(pecasImages[i-7] == vez) numJogadasPoss.add(i-1)
     }
 
 
