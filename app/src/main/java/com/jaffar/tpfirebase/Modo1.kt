63a59701659bad1cd0ab52f575cdce1dd8d184d6
andre170594 <andre_pessoa99@hotmail.com>
2022-01-02 04:31:22 +0000

    updtadegridFinal

--- app/src/main/java/com/jaffar/tpfirebase/Modo1.kt
+++ app/src/main/java/com/jaffar/tpfirebase/Modo1.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.core.view.get
 import com.jaffar.tpfirebase.databinding.ActivityModo1Binding
 
 
@@ -16,32 +17,43 @@ class Modo1 : AppCompatActivity(){
     lateinit var gridView: GridView
     var pecasImages = ArrayList<Int>(64)
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindModo1 = ActivityModo1Binding.inflate(layoutInflater)
         setContentView(bindModo1.root)
 
-
        for(i in 0 until 64){
-           if(i == 27 || i == 36)
-            pecasImages.add(R.drawable.x)
-            else
-           if(i == 28 || i == 35)
+           if(i == 27 || i == 36){
+               pecasImages.add(R.drawable.x)
+
+           } else if(i == 28 || i == 35){
                pecasImages.add(R.drawable.y)
-           else
+           }
+           else{
                pecasImages.add(R.drawable.blank)
+           }
+
        }
 
-
         gridView = bindModo1.gridView
-
-
 
         val pecasAdapter = PecasAdapter(this,pecasImages)
         gridView.adapter = pecasAdapter
 
         gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, pos, _ ->
-            pecasImages.set(27,R.drawable.x)
+
+
+            // aqui temos de ver qual é a vez de jogar para escolher qual letra meter
+
+            pecasImages.set(pos,R.drawable.x)
+
+
+            val pecasAdapter = PecasAdapter(this,pecasImages)
+            gridView.adapter = pecasAdapter
+
+
+
         }
 
 
